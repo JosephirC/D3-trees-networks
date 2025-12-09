e9d1841c461d6d4371af4a4734acadeebc8b715f
youssef <youssef.abida@etu.univ-lyon1.fr>
2025-12-09 11:53:26 +0100

    fin partie 1

---
+++ src/script.js
@@ -0,0 +1,25 @@
+import createAdjacencyMatrix from './matrix_management.js';
+
+// Definition de la taille du svg
+const margin = { top: 0, right: 30, bottom: 20, left: 10 },
+    width = 1960,
+    height = 960;
+
+// ajout du svg à une 'div id="matrice"' déjà créee dans la page html
+const svg = d3
+    .select("#matrice")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//Chargement des données
+d3.json("data/got_social_graph.json").then((d) => {
+    const nodes = d.nodes;
+    const edges = d.links;
+
+    const adjacency_matrix = createAdjacencyMatrix(nodes, edges);
+    console.log("Matrice d'adjacence créée", adjacency_matrix);
+
+});
